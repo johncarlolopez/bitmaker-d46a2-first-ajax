afb956a335051c57ad55b4acea87661584bea449
John Carlo Lopez <condottiere.jc@gmail.com>
2018-02-13 13:37:21 -0500

    complete step 7

--- ajax.js
+++ ajax.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
   var stepOneEle = document.querySelector('.step_one');
   var stepThreeEle = document.querySelector('.step_three');
   var stepThreeDivEle = document.querySelector('#step3456');
+  var stepSevenEle = document.querySelector('.step_seven');
+  var stepSevenDivEle = document.querySelector('#step7');
 
   stepOneEle.addEventListener('click', function() {
     var response = $.ajax( {
@@ -31,5 +33,16 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("Hey the request is finished!");
       });
   });
+
+  stepSevenEle.addEventListener('click', function() {
+    var response = $.ajax( {
+          url: 'https://first-ajax-api.herokuapp.com/count',
+          method: 'GET',
+          dataType: 'text'
+      }).done(function(responseData) {
+        var countResponseEle = document.createElement('p').innerHTML = responseData;
+        stepSevenDivEle.append(countResponseEle);
+      });
+  })
 
 });
